dddad0de76e28c81a33f2ee1df632c8d0a5ee905
Paul Williams <PaulNWms@github.com>
2023-10-17 11:33:50 -0700

    powershell-yaml

--- Update-Factoids.ps1
+++ Update-Factoids.ps1
@@ -1,7 +1,10 @@
+Import-Module powershell-yaml
+
 Add-Type -AssemblyName System.Core
 
-$sourceDir = "..\..\..\Documents\Vault\Zettelkasten"
-$targetDir = ".\content\study\Factoids"
+$sourceDir = "$PSScriptRoot\..\..\..\Documents\Vault\Zettelkasten"
+$tempDir = "$PSScriptRoot\temp"
+$targetDir = "$PSScriptRoot\content\study\Factoids"
 $mdFiles = Get-ChildItem "$sourceDir\*.md"
 $copied = 0
 $skipped = 0
@@ -9,7 +12,9 @@ $skipped = 0
 class Page {
     [string]$Name
     [string]$FileName
-    [string]$Slug
+    [string]$TargetPath
+    [string[]]$Yaml
+    [Hashtable]$Properties
     [Page]$Parent
     [System.Collections.Generic.List[Page]]$Children
     static [System.Collections.Generic.SortedDictionary[string,Page]]$Visited
@@ -17,6 +22,21 @@ class Page {
     Page([System.IO.FileInfo]$fileInfo) {
         $this.FileName = $fileInfo.FullName
         $this.Name = [Page]::LinkToFile($fileInfo.Name)
+        $this.Yaml = @()
+        [bool]$yamlStarted = $false
+        foreach ($line in (Get-Content $this.FileName)) {
+            if ($yamlStarted -and $line -match '^---') {
+                break
+            }
+            elseif (-not $yamlStarted -and $line -match '^---') {
+                $yamlStarted = $true
+                continue
+            }
+
+            $this.Yaml += $line
+        }
+
+        $this.Properties = ConvertFrom-Yaml ($this.Yaml | Join-String -Separator "`r`n")
     }
 
     static [string] LinkToFile([string]$inputString) {
@@ -25,7 +45,9 @@ class Page {
         if ($formattedString -match $regex) {
             $formattedString = $Matches[1]
         }
-        $formattedString = $formattedString -replace '\W', '-'        
+        $formattedString = $formattedString -replace '\W', '-'
+        $formattedString = $formattedString -replace '[-]+', '-'
+        $formattedString = $formattedString -replace '-$', ''
         return $formattedString
     }
 
@@ -33,26 +55,32 @@ class Page {
         return $this.Children.Count
     }
 
-    [void] AssignSlug() {
+    [void] AssignTargetPath() {
         [Page]::Visited = @{}
-        $this.AssignSlugHelper()
+        $this.AssignTargetPathHelper()
     }
 
-    [void] AssignSlugHelper() {
-        [Page]::Visited[$this.Name] = $this
-
-        if ($this.Parent) {
-            $this.Slug = $this.Parent.Slug + '/' + $this.Name
-        } else {
-            $this.Slug = $this.Name
+    [void] AssignTargetPathHelper() {
+        if ([Page]::Visited[$this.Name]) {
+            Write-Host "$($this.Name) already visited"
+            return
         }
 
-        foreach ($child in $this.Children) {
-            if (-not [Page]::Visited[$child.Name]) {
-                $child.AssignSlugHelper()
+        if ($this.Parent) {
+            if ($this.Parent.TargetPath) {
+                $this.TargetPath = Join-Path $this.Parent.TargetPath $this.Name
             } else {
-                Write-Host "$($child.Name) already visited"
+                $this.Parent.AssignTargetPathHelper()
+                return
             }
+        } else {
+            $this.TargetPath = $this.Name
+        }
+
+        [Page]::Visited[$this.Name] = $this
+
+        foreach ($child in $this.Children) {
+            $child.AssignTargetPathHelper()
         }
     }
 
@@ -61,44 +89,50 @@ class Page {
     }
 }
 
-# 1) Copy files
+# 1) Copy files to temp dir
+Remove-Item -Recurse $tempDir
+New-Item -ItemType Directory $tempDir
 foreach ($mdFile in $mdFiles) {
-    foreach ($line in (Get-Content $mdFile)) {
-        if ($line -match 'tags:') {
-            if ($line -match 'private') {
-                Write-Host "Skipping $($mdFile.Name)"
-                $skipped++
-            } else {
-                Copy-Item -Force $mdFile.FullName (Join-Path $targetDir $mdFile.Name)
-                $copied++
-            }
-            break
-        }
+    $sourcePage = [Page]::new($mdFile)
+    if ($sourcePage.Properties["tags"] -contains 'private') {
+        Write-Host "Skipping $($mdFile.Name)"
+        $skipped++
+    } else {
+        Copy-Item -Force $mdFile.FullName (Join-Path $tempDir $mdFile.Name)
+        $copied++
     }
 }
 
 Write-Host "Copied: $copied, Skipped: $skipped"
 
 # Populate hash
-Push-Location $targetDir
-$mds = Get-ChildItem *.md
+$mds = Get-ChildItem -Path $tempDir -Filter *.md
 [System.Collections.Generic.SortedDictionary[string,Page]]$pages = @{}
 foreach ($md in $mds) {
     $page = [Page]::new($md)
     $pages[$page.Name] = $page
+    foreach ($line in $page.Yaml) {
+        if ($line -match 'parent:\s*(.*)') {
+            $parent = [Page]::LinkToFile($Matches[1])
+            $page.Parent = $pages[$parent]
+            break
+        }
+    }
 }
 
 # Decorate hash with parents and children, and rewrite links to standard markdown format
 $regex = '\[\[(.*?)]]'
 foreach ($page in $pages.Values) {
     $lines = Get-Content $page.FileName
-    for($i = 0; $i -lt $lines.Count; $i++) {
+    for ($i = 0; $i -lt $lines.Count; $i++) {
         while ($lines[$i] -match $regex) {
             $link = $Matches[1]
             $target = [Page]::LinkToFile($link)
             $child = $pages[$target]
             if ($child) {
-                $child.Parent = $page
+                if (-not $child.Parent) {
+                    $child.Parent = $page
+                }
                 $page.Children += $child
             }
             $target = "../$target/"
@@ -113,12 +147,12 @@ $subjects = $pages.GetEnumerator() |
     Where-Object { -not $_.Value.Parent } |
     ForEach-Object { $_.Value }
 
-$subjects | select -Property Name, Slug, Parent, Children | ft
+$subjects | select -Property Name, TargetPath, Parent, Children, Yaml | fl
 
 foreach ($subject in $subjects) {
-    $pages[$subject.Name].AssignSlug()
+    $pages[$subject.Name].AssignTargetPath()
 }
 
-$pages.Values | select -Property Name, Slug, Parent, Children | ft
+$pages.Values | select -Property Name, TargetPath, Parent, Children | ft
 
 Pop-Location
